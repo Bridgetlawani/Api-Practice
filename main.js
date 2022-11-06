ae58940bd1676a5c4cde57ca400a2d301a839f1e
Bridget <bridgetlawani09@gmail.com>
2022-11-06 23:16:19 +0100

    modified pages

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+let timer
+let deleteFirstPhotoDelay
+
 async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
-    createBreedList(data.message)
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all")
+        const data = await response.json()
+        createBreedList(data.message)
+
+    } catch (e){
+        console.log("There was a problem fetching the Dog breeds")
+
+    }
+    
 
 }
 start()
@@ -27,9 +37,38 @@ async function loadByBreed(breed) {
     }
 
 }
+
 function createSlideshow(images) {
-    document.getElementById("slideshow").innerHTML = `
-    <div class="slide" style="background-image: url('${images}[0]');"></div>
-    
-    `
-} 
+    let currentPosition = 0
+    clearInterval(timer)
+    clearTimeout(deleteFirstPhotoDelay)
+    if (images.length > 1) {
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide" style="background-image: url('${images[1]}')"></div>
+        `
+    currentPosition += 2
+    if(images.length == 2) currentPosition = 0
+     timer = setInterval(nextSlide, 3000)
+
+    } else {
+        document.getElementById("slideshow").innerHTML = `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide"></div>
+        `
+    }
+
+    function nextSlide() {
+        document.getElementById("slideshow").insertAdjacentHTML("beforeend", `    <div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+        deleteFirstPhotoDelay = setTimeout(function() {
+            document.querySelector(".slide").remove()
+        }, 1000)
+        if (currentPosition + 1 >= images.length) {
+            currentPosition = 0
+
+        } else{
+            currentPosition++
+
+        }
+    }
+}
